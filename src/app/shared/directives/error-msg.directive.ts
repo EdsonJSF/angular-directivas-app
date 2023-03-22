14a91b30e61fdac18a6c3e9a18dc6bbb96340be2
Edsonjsf <edsonjsf1990@gmail.com>
2023-03-22 13:16:39 -0400

    Cambiar el mensaje de la etiqueta

--- src/app/shared/directives/error-msg.directive.ts
+++ src/app/shared/directives/error-msg.directive.ts
@@ -7,6 +7,7 @@ export class ErrorMsgDirective implements OnInit {
   private htmlElement!: ElementRef<HTMLElement>;
 
   @Input() color: string = 'red';
+  @Input() mensaje: string = 'Este campo es necesario';
 
   constructor(private el: ElementRef<HTMLElement>) {
     this.htmlElement = el;
@@ -14,9 +15,14 @@ export class ErrorMsgDirective implements OnInit {
 
   ngOnInit(): void {
     this.setColor();
+    this.setMensaje();
   }
 
   setColor() {
     this.htmlElement.nativeElement.style.color = this.color;
+  }
+
+  setMensaje() {
+    this.htmlElement.nativeElement.innerText = this.mensaje;
   }
 }
