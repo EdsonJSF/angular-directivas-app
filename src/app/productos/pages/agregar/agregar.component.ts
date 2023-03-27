f86f62edd7276484acdc02ed374c4f0aea2c7d3b
EdsonJSF <edsonjsf1990@gmail.com>
2023-03-27 18:32:15 -0400

    Input setters

--- src/app/productos/pages/agregar/agregar.component.ts
+++ src/app/productos/pages/agregar/agregar.component.ts
@@ -11,6 +11,9 @@ export class AgregarComponent {
     nombre: ['', [Validators.required]],
   });
 
+  texto: string = 'Debe de ingresar este campo';
+  color: string = 'red';
+
   constructor(private fb: FormBuilder) {}
 
   validFormByName(name: string) {
@@ -19,5 +22,16 @@ export class AgregarComponent {
       this.miFormulario.get(name)?.touched;
 
     return invalid || false;
+  }
+
+  cambiarTexto() {
+    this.texto = Math.random().toString();
+  }
+
+  cambiarColor() {
+    const color = '#xxxxxx'.replace(/x/g, (y) =>
+      ((Math.random() * 16) | 0).toString(16)
+    );
+    this.color = color;
   }
 }
--- src/app/shared/directives/error-msg.directive.ts
+++ src/app/shared/directives/error-msg.directive.ts
@@ -1,28 +1,59 @@
-import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 
 @Directive({
   selector: '[appErrorMsg]',
 })
-export class ErrorMsgDirective implements OnInit {
+export class ErrorMsgDirective implements OnInit, OnChanges {
   private htmlElement!: ElementRef<HTMLElement>;
 
-  @Input() color: string = 'red';
-  @Input() mensaje: string = 'Este campo es necesario';
+  private _color = 'red';
+  private _mensaje = 'Este campo es requerido';
+
+  @Input() set color(valor: string) {
+    this.htmlElement.nativeElement.style.color = valor;
+    this._color = valor;
+  }
+
+  @Input() set mensaje(valor: string) {
+    this.htmlElement.nativeElement.innerText = valor;
+    this._mensaje = valor;
+  }
 
   constructor(private el: ElementRef<HTMLElement>) {
     this.htmlElement = el;
   }
 
   ngOnInit(): void {
-    this.setColor();
-    this.setMensaje();
+    this.setEstilo();
   }
 
-  setColor() {
-    this.htmlElement.nativeElement.style.color = this.color;
+  ngOnChanges(changes: SimpleChanges): void {
+    // if (changes['mensaje']) {
+    //   this.htmlElement.nativeElement.innerText =
+    //     changes['mensaje'].currentValue;
+    // }
+    // if (changes['color']) {
+    //   this.htmlElement.nativeElement.style.color =
+    //     changes['color'].currentValue;
+    // }
   }
 
-  setMensaje() {
-    this.htmlElement.nativeElement.innerText = this.mensaje;
+  setEstilo() {
+    this.htmlElement.nativeElement.classList.add('form-text');
   }
+
+  // setColor() {
+  //   this.htmlElement.nativeElement.style.color = this.color;
+  // }
+
+  // setMensaje() {
+  //   this.htmlElement.nativeElement.innerText = this.mensaje;
+  // }
 }
